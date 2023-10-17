f5fdb791a10d80613431e15ed6264a2cbafaadcb
Saswat Mahapatra <92862073+logan1o1@users.noreply.github.com>
2023-10-17 22:34:03 +0530

    portfolio

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <p>Contact information and a form here.</p>
+    </section>
+  );
+}
+
+export default Contact;
